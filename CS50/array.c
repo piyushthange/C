f27c1a60d1835c916e8fed20226c38f44113c94a
Piyush Thange <pthange19@gmail.com>
2021-02-05 22:45:08 +0530

    Changing the array program with some constant variable
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- CS50/array.c
+++ CS50/array.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
+const int TOTAL = 3; 
+float average(int length, int array[]);
+
+int main() 
+{ 
 	int scores[4];
 	for (int i = 0; i < 4; i++)
 		scanf("%d\n", &scores[i]);
+	
+	printf("Average: %f\n", average(TOTAL, scores));
+}
+
+float average(int length, int array[])
+{
+	int sum = 0;
+	for (int i = 0; i < length; i++)
+		sum = sum + array[i];
+	return sum / (float) length;
 }
