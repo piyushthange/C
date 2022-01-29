71aa7aa74e767c6b844b9a47542cd9b679599b82
Piyush Thange <pthange19@gmail.com>
2022-01-29 09:40:00 +0530

    character pointer vs array char
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ test/6_char_ptr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *agrv[]){
+	char str[20] = "hello start";
+	char *ptr = "hello pointer";
+
+	printf("str array = %s\n", str);
+	printf("pointer char = %s\n", ptr);
+
+	return 0;
+}
