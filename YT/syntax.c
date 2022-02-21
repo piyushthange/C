6a66ef7ac56146476ea0832d8f4393464c374494
Piyush Thange <pthange19@gmail.com>
2022-02-21 22:52:11 +0530

    Syntax for C
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ YT/syntax.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(){
+	/* write out to the screen */
+	/* read from the user */
+	/* variables */
+	/* if-else if-else conditon */
+	/* Arithmetic operations */
+	/* Relational & Logical operators */
+	/* type conversion */
+	/* Increment and decrement operator */
+	/* Arrays */
+	/* Function */
+	/* Pointer*/
+	/* Structure*/
+	/* File Operations */
+}
