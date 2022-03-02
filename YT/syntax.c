0bd009584b593bc18e67d3857572b69166337c8c
Piyush Thange <pthange19@gmail.com>
2022-03-02 22:42:42 +0530

    updating file
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- YT/syntax.c
+++ YT/syntax.c
@@ -3,13 +3,21 @@
 int main(int argc, char *argv[])
 {
 	/* write out to the screen */
+	/* Data types and it's place holder */
+	int i = 2;
+	float j = 33.2;
+	
 	/* read from the user */
-	/* variables */
+	/* comments */
+	/* escape sequences */
 	/* if-else if-else conditon */
+	/* switch () */
+	/* while, do while, for */
 	/* Arithmetic operations */
 	/* Relational & Logical operators */
 	/* type conversion */
 	/* Increment and decrement operator */
+
 	/* Arrays */
 	/* Function */
 	/* Pointer*/
