a4df4f85e11909e0335ca69e0cc99fb80d61d6ba
Piyush Thange <pthange19@gmail.com>
2022-03-03 23:09:07 +0530

    update
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- YT/syntax.c
+++ YT/syntax.c
@@ -4,9 +4,6 @@ int main(int argc, char *argv[])
 {
 	/* write out to the screen */
 	/* Data types and it's place holder */
-	int i = 2;
-	float j = 33.2;
-	
 	/* read from the user */
 	/* comments */
 	/* escape sequences */
