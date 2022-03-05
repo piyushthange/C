90c74f8dc806fffdefcee1a471ff1b2c472e2963
Piyush Thange <pthange19@gmail.com>
2022-03-05 23:57:56 +0530

    adding data types
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- YT/syntax.c
+++ YT/syntax.c
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 	/* write out to the screen */
 	printf("Hello, World!!");
 	/* Data types and it's place holder */
+	int i = 4;
+	float j = 2.3;
+	double k = 23234.23434;
+	char = 'a'
+	
 	/* read from the user */
 	/* comments */
 	/* escape sequences */
