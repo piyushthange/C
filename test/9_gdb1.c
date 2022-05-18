bd91b741bc51a19bbe5153ce31f7efc7b52f5f25
Piyush Thange <pthange19@gmail.com>
2022-05-18 11:54:36 +0530

    gdb 1
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ test/9_gdb1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	char *day[7] = {
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday"
+	};
+
+	for (i = 0; i < 7; i++)
+		printf("day[%d] = %s\n", i, day[i]);
+	
+	return 0;
+}
