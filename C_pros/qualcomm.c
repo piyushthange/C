c715948b85b4ab7812437f428832a5d0503b6e93
Piyush Thange <pthange19@gmail.com>
2022-08-31 18:36:39 +0530

    Qualcomm test
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ C_pros/qualcomm.c
@@ -0,0 +1,79 @@
+Input : number = 171
+             k = 5 
+             p = 2
+Output : The extracted number is 21
+171 is represented as 10101011 in binary,
+so, you should get only 10101 i.e. 21.
+
+
+#include <stdio.h>
+
+int s;
+static int s;
+
+extern int s;
+
+int main()
+
+
+void bin(int num, int k, int p) {
+	int i, rem, base = 1, bin = 0, bin2 = 0;
+  
+  i = num;
+  
+  while(i > 0) {
+  	rem = i % 2
+    bin = bin + rem * base;
+    i = i / 2;
+    base = base * 10;
+  }
+  
+  //bin - binary;
+  
+  for (i = p; i < k + 2; i++) {
+  	bin2 = bin2 + bin
+  }
+}
+
+
+
+
+#define R 10
+#define C 10
+
+
+int main(){
+	int a, b, c[3][3];
+  c[][] = (int *)malloc(int * 9);
+  
+  scanf("%d %d", &a, &b);
+}
+
+
+bcdefghabc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
