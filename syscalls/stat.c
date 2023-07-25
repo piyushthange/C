ee455da0e3a40803f096587a6ffa0caa809d8647
Piyush Thange <pthange19@gmail.com>
2023-07-25 21:56:28 +0530

    modifying stat
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- syscalls/stat.c
+++ syscalls/stat.c
@@ -3,14 +3,20 @@
 
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 int main()
 {
 	struct stat buff;
+	struct stat a;
 	stat("hello.txt", &buff);
+	stat("open.c", &a);
 
 	int ino = buff.st_ino;
+	int uid = a.st_uid;
 	printf("inode : %d\n", ino);
+	printf("uid : %d\n", uid);
+	
 
 	return 0;
 }
