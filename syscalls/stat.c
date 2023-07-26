c279633f044723f30f8f219fcd168b09ccbdd3eb
Piyush Thange <pthange19@gmail.com>
2023-07-26 22:04:52 +0530

    Stat using pointer
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- syscalls/stat.c
+++ syscalls/stat.c
@@ -8,15 +8,14 @@
 int main()
 {
 	struct stat buff;
-	struct stat a;
+	struct stat *a = (struct stat *)malloc(sizeof(struct stat));
 	stat("hello.txt", &buff);
-	stat("open.c", &a);
+	stat("open.c", a);
 
 	int ino = buff.st_ino;
-	int uid = a.st_uid;
+	int uid = a->st_uid;
 	printf("inode : %d\n", ino);
 	printf("uid : %d\n", uid);
-	
-
+	printf("link : %ld\n", a->st_nlink);
 	return 0;
 }
