e30f4e8d9b8a6d7daf3034fc79b9261445142a2a
Piyush Thange <pthange19@gmail.com>
2024-02-16 22:07:23 +0530

    Adding linked list dir with printing function
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ linkedList/1_linked.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int i;
+	struct node *next;
+}node;
+
+void printll(node *head) {
+	node *ptr;
+	ptr = NULL;
+	ptr = head;
+
+	while (ptr != NULL) {
+		printf("%d ", ptr->i);
+		ptr = ptr->next;
+	}
+
+	printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+	node *head = (struct node *)malloc(sizeof(node));
+	head->i = 23;
+
+	printll(head);
+}
