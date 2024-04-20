beb3b9b53559207797e368a3566d674df69f9088
Piyush Thange <pthange19@gmail.com>
2024-04-20 14:12:56 +0530

    simple prime
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- prep/2_basics.c
+++ prep/2_basics.c
@@ -2,25 +2,29 @@
 
 int sum(int n);
 int factorial(int a);
+int prime(int a);
 
 int main(int argc, char *argv[]) {
 	
 	//print power to int
-	int a, pow, num;
-	printf("Enter a Number & power: ");
-	scanf("%d", &a);
-	scanf("%d",&pow);
+	//int a, pow, num;
+	//printf("Enter a Number & power: ");
+	//scanf("%d", &a);
+	//scanf("%d",&pow);
 	//scanf ("%d %d", &a, &pow);
-	num = a;
+	//num = a;
 
-	for (int i=1; i < pow; i++){
-		num = num * a;
-	}
-	printf ("%d\n", num);
-
-	int asdf = sum(3);
-	printf("%d\n", asdf);
-	factorial(4);
+//	for (int i=1; i < pow; i++){
+//		num = num * a;
+//	}
+//	printf ("%d\n", num);
+//
+//	int asdf = sum(3);
+//	printf("%d\n", asdf);
+	//factorial(4);
+	prime(7);
+	prime(12);
+	prime(1);
 
 	return 0;
 }
@@ -42,4 +46,22 @@ int factorial(int a) {
 		s = s * i;
 	}
 
+}
+
+int prime(int a) {
+	int s = a;
+	if (s < 2) {
+		printf("Nor prime nor composite\n");
+		return 0;
+	}else {
+		for (int i = 2; i < s; i++) {
+			if (s % i == 0) {
+				printf("Not prime no\n");
+				return 0;
+			}
+		}
+
+		printf("Prime Number\n");
+		return 0;
+	}
 }
