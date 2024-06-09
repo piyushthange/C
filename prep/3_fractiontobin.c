6d68023a5c06addc74559f7d0d41f963b54b2c1c
Piyush Thange <pthange19@gmail.com>
2024-06-09 22:24:29 +0530

    fratrion to bin
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ prep/3_fractiontobin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void ftob(double a);
+int main(int argc, char *argv[])
+{
+	double a;
+	printf("Enter Fractional number: ");
+	scanf("%d\n", &a);
+	ftob(a);
+	return 0;
+}
+
+void ftob(double a) 
+{
+	
+}
