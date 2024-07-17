5f42c92048975605daa1aae4cae17ccd17e0e331
Piyush Thange <pthange19@gmail.com>
2024-07-17 22:00:37 +0530

    constructors parameterised, COPY, non parameterised
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ OOP/constructor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Customer{
+	string name;
+	int acc;
+	int bal;
+
+public:
+	//Non  parameterised constructor
+	Customer() {
+		name = "Abc";
+		acc = 1234;
+		bal = 9089;
+		cout << "This is constructor\n";
+	}
+	
+	//Parameterised constructor
+
+//	Customer(string name, int acc, int bal) {
+//		this->name = name;
+//		this->acc = acc;
+//		this->bal = bal;
+//	}
+
+	//Inline Cosntructor
+
+	inline Customer(string s, int a, int b): name(s), acc(a), bal(b){
+
+	}
+
+	//Display function
+	void display() {
+		cout << "Name: " << name  << endl;
+		cout << "Account: "<< acc << endl;
+		cout << "Balance: " << bal << endl;
+	}
+};
+
+int main()
+{
+	Customer d("Consturctor", 19884, 99842);
+	d.display();
+
+}
