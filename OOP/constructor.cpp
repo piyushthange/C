2836c390c186ba8a09d958fa1553ac089bf3bc13
Piyush Thange <pthange19@gmail.com>
2024-07-18 21:55:29 +0530

    copy constructor
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- OOP/constructor.cpp
+++ OOP/constructor.cpp
@@ -17,16 +17,22 @@ public:
 	
 	//Parameterised constructor
 
-//	Customer(string name, int acc, int bal) {
-//		this->name = name;
-//		this->acc = acc;
-//		this->bal = bal;
-//	}
+	Customer(string name, int acc, int bal) {
+		this->name = name;
+		this->acc = acc;
+		this->bal = bal;
+	}
 
 	//Inline Cosntructor
 
-	inline Customer(string s, int a, int b): name(s), acc(a), bal(b){
+//	inline Customer(string s, int a, int b): name(s), acc(a), bal(b){	}
 
+	// Copy Constructor
+
+	Customer(Customer &obj) {
+		name = obj.name;
+		acc = obj.acc;
+		bal = obj.bal;
 	}
 
 	//Display function
@@ -40,6 +46,8 @@ public:
 int main()
 {
 	Customer d("Consturctor", 19884, 99842);
+	Customer copy_con(d);
 	d.display();
+	copy_con.display();
 
 }
