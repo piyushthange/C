af946215f5d641a2edba5516419ee807c9ada91d
Piyush Thange <pthange19@gmail.com>
2024-08-23 17:58:40 +0530

    Hierarchical Inheriance
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ OOP/hierarchical_inheritance.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Human {
+public:	
+	string specilization;
+
+	void work() {
+		cout << "working spec: " << specilization;
+	}
+};
+
+class Youtuber {
+public:	
+	int subscribers;
+
+	void contentcreator() {
+		cout << "Subs base " << subscribers <<endl;
+	}
+};
+
+class CodeTeacher: public Engineer, public Youtuber {
+public:
+	string name;
+
+	CodeTeacher(string name, string specilization, int subscribers) {
+		this->name = name;
+		this->specilization = specilization;
+		this->subscribers = subscribers;
+	}
+
+	void showcase() {
+		cout << "My name is " << name;
+		work();
+		contentcreator();
+	}
+};
+
+int main() {
+	
+	CodeTeacher A1("Rk", "ssw", 9);
+	A1.showcase();
+}
