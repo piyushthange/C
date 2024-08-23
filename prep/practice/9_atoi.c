a468414cd3fd7eb8a78cd4ebe949b74c6f31cca3
Piyush Thange <pthange19@gmail.com>
2024-08-23 18:04:23 +0530

    atoi
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- prep/practice/9_atoi.c
+++ prep/practice/9_atoi.c
@@ -1,40 +1,20 @@
 /* atoi: convert s to integer */
-//#include <stdio.h>
-//
-//int atoi(char s[]);
-//int main()
-//{
-//	char a[] = "asdlf4234";
-//	printf("%d\n", atoi(a));
-//}
-//
-//int atoi(char s[])
-//{
-//	int i, n;
-//	n = 0;
-//	for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
-//		n = 10 * n + (s[i] - '0');
-//	
-//	return n;
-//}
-
 #include <stdio.h>
 
-int atoi(char s);
-
-int main(int arc, char *argv[])
+int atoi(char s[]);
+int main()
 {
-	char *a = "1234";
-	atoi(a);
-
-	return 0;
-
+	char a[] = "04234asf";
+	printf("%d\n", atoi(a));
 }
 
-int atoi(char s){
-	int n;
-	for (int i =0; *s[i] >= '0' && *s[i] <= '9'; i++) {
-		n = 10 * n + (*s[i] - '0');
-	}
+int atoi(char s[])
+{
+	int i, n;
+	n = 0;
+	for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+		n = 10 * n + (s[i] - '0');
+	
 	return n;
 }
+
