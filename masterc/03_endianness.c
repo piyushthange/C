8ce37c046560aa35c24082ee9c948f49fbdebab9
Piyush Thange <pthange19@gmail.com>
2024-09-02 18:15:15 +0530

    Endianness using UNION
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/03_endianness.c
+++ masterc/03_endianness.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+union endian_check {
+	char c;
+	unsigned int i;
+};
+
 int main(int argc, char *argv[]) {
 	unsigned int i = 1;
 	char *c = (char *)&i;
@@ -10,5 +15,14 @@ int main(int argc, char *argv[]) {
 	else
 		printf("Big Endian\n");
 	
+
+	// endian by union
+	union endian_check un;
+	un.i = 0x01;
+	
+	if (un.c == 1)
+		printf("Little Endian\n");
+	else
+		printf("Big Endian\n");
 	return 0;
 }
