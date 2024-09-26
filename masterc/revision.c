67aa450507469233556aa8082b5568330f9f44d4
Piyush Thange <pthange19@gmail.com>
2024-09-26 21:48:38 +0530

    Delete at postion for linked List
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/revision.c
+++ masterc/revision.c
@@ -7,9 +7,15 @@ typedef struct node {
 	struct node *next;
 }node;
 
+node *add_begin(node *head, int i); //pass by value
+void add_to_begin(node **head, int d); //pass by reference
+
 node *add_end(node *ptr, int i);
-node *add_begin(node *head, int i);
-node *del(node *head);
+node *del(node *head); //delete complete list
+node *del_first_node(node *head);
+node *del_last_node(node *head);
+void del_last_node_single_pointer(node *head);
+void del_at_pos(node *head, int pos);
 node *ins_at_pos(node *head, int i, int pos);
 node *rev(node *head);
 void pr(node *head);
@@ -27,21 +33,42 @@ int main() {
 	ptr = add_end(ptr, 50);
 	ptr = add_end(ptr, 60);
 
+	printf("Addded Elemenst : ");
 	pr(head);
 
+	printf("Insert at position : ");
 	ins_at_pos(head, 2, 2);
-
 	pr(head);
 
+	printf("Reverse Linked List : ");
 	head = rev(head);
 	pr(head);
 
+	printf("Add node at Begining using PASS BY VALLUE\n");
 	head = add_begin(head, 55);
 	pr(head);
 
-	head = del(head);
+	printf("Add node at Begining using PASS BY REFERENCE\n");
+	add_to_begin(&head, 12);
 	pr(head);
 
+	printf("Delete First Node : ");
+	head = del_first_node(head);
+	pr(head);
+
+	printf("Delete Last Node : ");
+	head = del_last_node(head);
+	pr(head);
+
+	printf("Del last node using single pointer : ");
+	del_last_node_single_pointer(head);
+	pr(head);
+	
+	del_at_pos(head, 2);
+
+	printf("----> Deleting Linked list <---- ");
+	head = del(head);
+	pr(head);
 	if(head == NULL) 
 		printf("Linked List Deleted Successfully\n");
 
@@ -53,9 +80,75 @@ node *add_begin(node *head, int i) {
 	ptr->i = i;
 	ptr->next = NULL;
 
-	ptr->next = head->next;
+	ptr = head;
 	head = ptr;
 	return head;
+}
+
+void add_to_begin(node **head, int i) {
+	node *ptr = (node *)malloc(sizeof(node));
+	ptr->i = i;
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+}
+
+node *del_first_node(node *head) {
+	if(head == NULL)
+		printf("List is Empty\n");
+
+	node *ptr = head->next;
+	free(head);
+	head = ptr;
+	return head;
+}
+
+// void del_last_node(node *head) can be used & at end remove return from functon
+node *del_last_node(node *head) {
+	node *ptr = head; //second last ptr
+	node *last_ptr = head;
+
+	if (head == NULL)
+		printf("List is empty\n");
+	else if (head->next == NULL) {
+		free(head);
+		head = NULL;
+	} else {
+		while(last_ptr->next != NULL) {
+			ptr = last_ptr;
+			last_ptr = last_ptr->next;
+		}
+		ptr->next = NULL;
+		free(last_ptr);
+		last_ptr = NULL;
+	}
+	return head;
+}
+
+//delete last node using single pointer
+void del_last_node_single_pointer(node *head) {
+	node *ptr = head;
+
+	while(ptr->next->next != NULL) {
+		ptr = ptr->next;
+	}
+	free(ptr->next);
+	ptr->next = NULL;
+}
+
+void del_at_pos(node *head, int pos) {
+	node *ptr = head;
+	node *del = head;
+
+	pos--;
+	while(pos != 1) {
+		ptr = ptr->next;
+		pos--;
+	}
+	del = ptr;
+	ptr = ptr
+	printf("%d \n", ptr->i);
 }
 
 node *rev(node *head) {
