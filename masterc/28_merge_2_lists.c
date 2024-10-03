3b425aa5384694d59504b6251a39cc573beeff74
Piyush Thange <pthange19@gmail.com>
2024-10-03 22:27:27 +0530

    Created list 1
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/28_merge_2_lists.c
+++ masterc/28_merge_2_lists.c
@@ -6,22 +6,41 @@ typedef struct node {
 	struct node *next;
 }node;
 
+node *add_end(node *ptr, int i);
+void pr(node *head);
+node *mergeTwoLists(node *list1, node *list2);
+
 int main() {
 	node *head = malloc(sizeof(node));
 	head->i = 1;
 	head->next = NULL;
 
 	node *ptr = head;
-	ptr = add_end(ptr,2)
+	ptr = add_end(ptr,2);
+	ptr = add_end(ptr,4);
+	ptr = add_end(ptr,6);
+	ptr = add_end(ptr,8);
+
+	pr(head);
 }
 
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+node *add_end(node *ptr, int i) {
+	node *tmp = (node *)malloc(sizeof(node));
+	tmp->i = i;
+	tmp->next = NULL;
+
+	ptr->next = tmp;
+	ptr = tmp;
+	return ptr;
+}
+
+node *mergeTwoLists(node *list1, node *list2) {
    if(list1 == NULL)
         return list2;
     if (list2 == NULL)
         return list1;
-    struct ListNode *t;
-    if(list1->val < list2->val) {
+    node *t;
+    if(list1->i < list2->i) {
         t = list1;
         list1 = list1->next;
     } else {
@@ -29,9 +48,9 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         list2 = list2->next;
     }
 
-    struct ListNode *head = t;
+    node *head = t;
     while(list1 != NULL && list2 != NULL){
-        if(list1->val <= list2->val) {
+        if(list1->i <= list2->i) {
             t->next = list1;
             list1 = list1->next;
         } else {
@@ -47,5 +66,13 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         t->next = list1;
 
     return head;
-        
+}
+
+void pr(node *head) {
+	node *ptr = head;
+	while(ptr != NULL) {
+		printf("%d ", ptr->i);
+		ptr = ptr->next;
+	}
+	printf("\n");
 }
