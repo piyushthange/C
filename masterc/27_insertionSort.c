31222acd903f71afca934fe89c1a31c4896f47be
Piyush Thange <pthange19@gmail.com>
2024-10-05 21:37:43 +0530

    function
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/27_insertionSort.c
+++ masterc/27_insertionSort.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void insertion_sort(int *arr, int len);
+
 int main() {
 	int arr = {3,8,1,6,2,4,9,5,0};
 
 }
 
+void insertion_sort(int *arr, int len) {
+	
+}
