2a8c035aece27405c74828e43b351d4f8c06905c
Piyush Thange <pthange19@gmail.com>
2024-10-06 21:35:58 +0530

    Functions added
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/06_selectionSort.c
+++ masterc/06_selectionSort.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	int a[] = {1,3,7, 9, 0, 2, 8, 4, 5, 6}; 
-	//find lowest number
-	int len = sizeof(a) / sizeof(a[0]);
-	printf("Len %d\n", len);
-	for(int i = 0; i < len - 1; i++) {
-		int index = i;
-		for(int j=i+1; j < len; j++) {
-			if(a[j] < a[index])
-			index = j;
-		}
-		a[i] = a[i] ^ a[index];
-		a[index] = a[i] ^ a[index];
-		a[i] = a[i] ^ a[index];
-	}
-	for (int i = 0; i < len; i++) {
-		printf("%d ", a[i]);
-	}
+void selection_sort(int *a, int len);
+void pr(int *a, int len);
 
-	printf("\n\n");
-	end_selection(a, len);
-	printf("\n");
+int main(int argc, char *argv[]) {
+	int a[] = {2,5,3,9,8,1,7,4,6,0};
+	int len = sizeof(a) / sizeof(a[0]);
+	pr(a, len);
+	selection_sort(a, len);
+	pr(a, len);
 
 	return 0;
+}
+
+void selection_sort(int *a, int len) {
+	for(int i = 0; i < len-1; i++) {
+		int index = i;
+		for (int j = i+1; j < len; j++) {
+			if (a[j] < a[index])
+				index = j;
+		}
+		int tmp = a[i];
+		a[i] = a[index];
+		a[index] = tmp;
+	}
+}
+
+void pr(int *a, int len) {
+	for(int i = 0; i < len; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
 }
