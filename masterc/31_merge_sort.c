00141177ec66385be920133ffa7bea7142a4b86a
Piyush Thange <pthange19@gmail.com>
2024-10-12 22:49:55 +0530

    removed pr func
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- masterc/31_merge_sort.c
+++ masterc/31_merge_sort.c
@@ -4,12 +4,6 @@ int main() {
 	int a[] = {3,6,1,4,2,8,5,4,7,0,9};
 	int len = sizeof(a) / sizeof(a[0]);
 
-	merge_sort(a, len);
-
-	for(int i = 0; i < len; i++)
-		printf("%d ", a[i]);
-	printf("\n");
-
 	return 0;
 }
 
